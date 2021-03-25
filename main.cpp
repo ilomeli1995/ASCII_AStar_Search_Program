4d8e3e022a73586cc7fc1eb2004d2ee9d85aae9b
Ivan Ignacio Lomeli Rubio <ivanlomeli@MacBook-Pro.local>
2021-03-25 00:05:24 -0600

    Delete TODO comments in main.cpp

--- main.cpp
+++ main.cpp
@@ -54,7 +54,6 @@ vector< vector<State> > ReadBoardFile(string path)
   return board;
 }
 
-// TODO: Write the Search function stub here.
 vector< vector<State> > Search(vector< vector<State> > boardGrid, int start[2], int goal[2])
 {
   vector< vector<State>> path{};
@@ -89,16 +88,13 @@ void PrintBoard(const vector< vector<State> > board)
 
 int main() 
 {
-  // TODO: Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
   int init[2] = { 0,0 };
   int goal[2] = { 4,5 };
 
   auto board = ReadBoardFile("1.board");
 
-  // TODO: Call Search with "board", "init", and "goal". Store the results in the variable "solution".
   vector< vector<State> > solution = Search(board, init, goal);
 
-  // TODO: Change the following line to pass "solution" to PrintBoard.
   PrintBoard(solution);
 
   return 0;
